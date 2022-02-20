ec4f44c535edfaa31eb06053551100128698af33
Elliott Shugerman <eeshugerman@gmail.com>
2022-02-20 15:42:20 -0500

    add quit command

--- dconf-wrangler.el
+++ dconf-wrangler.el
@@ -38,8 +38,9 @@
                                                              (expand-file-name "~/.config"))
                                                          "dconf-user.conf")
   "Path to dconf config file to operate on")
-(defvar dconf-wrangler--source-buffer nil)
 
+(defvar dconf-wrangler--frame nil)
+(defvar dconf-wrangler--source-buffer nil)
 (defvar dconf-wrangler--target-buffer nil)
 
 (define-derived-mode dconf-wrangler-dump-mode special-mode "dconf dump"
@@ -58,12 +59,12 @@
          (target-buffer (get-buffer-create "*dconf-wrangler-target*"))
          (source-buffer (get-buffer-create (format "*dconf-wrangler-%s*" source-type))))
 
+    (setq dconf-wrangler--frame frame)
+    (setq dconf-wrangler--source-buffer source-buffer)
+    (setq dconf-wrangler--target-buffer target-buffer)
 
     (set-window-buffer target-window target-buffer)
     (set-window-buffer source-window source-buffer)
-
-    (setq dconf-wrangler--source-buffer source-buffer)
-    (setq dconf-wrangler--target-buffer target-buffer)
 
     (with-current-buffer target-buffer
       (insert-file-contents dconf-wrangler-dconf-config-file-path)
@@ -77,6 +78,16 @@
         (source-buffer dconf-wrangler--source-buffer))
     (shell-command command source-buffer "*Messages*")
     (with-current-buffer source-buffer (dconf-wrangler-dump-mode))))
+
+(defun dconf-wrangler-quit ()
+  (interactive)
+  ;; todo: only ask when there are actually changes
+  (when (y-or-n-p (format "Save changes to %s?"
+                          dconf-wrangler-dconf-config-file-path))
+    (write-region nil nil dconf-wrangler-dconf-config-file-path))
+  (kill-buffer dconf-wrangler--target-buffer)
+  (kill-buffer dconf-wrangler--source-buffer)
+  (delete-frame dconf-wrangler--frame))
 
 (provide 'dconf-wrangler)
 
